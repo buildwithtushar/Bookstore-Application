6957881540a928040a43bbced843e7a601591e0a
buildwithtushar <tushar.sinha.build@gmail.com>
2026-04-09 14:46:33 +0530

    Initial commit

---
+++ src/test/java/com/sinhtu8/bookstoreapplication/BookstoreApplicationTests.java
@@ -0,0 +1,11 @@
+package com.sinhtu8.bookstoreapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
